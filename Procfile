7aacff5e45fbcdfc82a19645cde3f202bd916d57
aleksey.zelepukin@gmail.com <Mol0nLab3gkb!>
2020-05-07 19:44:51 +0300

    Procfile updated

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn front:app
+web: gunicorn app:app
--- front.py
+++ front.py
@@ -157,4 +157,4 @@ def predict_form():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='5000', debug=True)
+    app.run(host='localhost', port='5000', debug=True)
